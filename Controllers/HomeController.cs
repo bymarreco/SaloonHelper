a33b10d7fff9536a55c690c0851e39bad1c7819e
Lucas Rech <bymarreco@gmail.com>
2021-02-12 15:45:50 -0300

    Adding Product class and page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SaloonHelper.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Calculate()
+        {
+            return View();
+        }
     }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,7 @@
+namespace SaloonHelper.Models
+{
+    public class Product
+    {
+        public int InitialPrice { get; set; }
+    }
+}

45fe40c6b9fd7285763cac637b02e75f30185340
Lucas Rech <bymarreco@gmail.com>
2021-02-12 15:26:39 -0300

    Adding Base Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaloonHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
